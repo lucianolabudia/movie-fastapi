d5d97fc4114438d7276f8511113b44ac5121545e
Luciano Labudia <labudia.luciano@gmail.com>
2023-01-21 13:00:45 -0300

    Autenticación

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# movie-fastapi
+# movie-FastAPI
+
+## Flujo de autenticación
+
+### Ruta para iniciar sesión
+Se obtiene como resultado la protección de determinadas rutas de la aplicación para las cuales solo se podrá acceder mediante el inicio de sesión del usuario. Para esto se crea una ruta que utilice el método POST donde se solicitarán los datos como email y contraseña.
+
+### Creación y envío de token
+Luego de que el usuario ingrese sus datos de sesión correctos este obtendrá un token que le servirá para enviarlo al momento de hacer una petición a una ruta protegida.
+
+### Validación de token
+Al momento de que la API reciba la petición del usuario, comprobará que este le haya enviado el token y validará si es correcto y le pertenece. Finalmente, se le dará acceso a la ruta que está solicitando.
+
+Se utiliza la librería pyjwt.
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI, Body, Path, Query
+from fastapi import FastAPI, Body, Path, Query, Request, HTTPException, Depends
 from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from jwt_manager import create_token, validate_token
+from fastapi.security import HTTPBearer
 
 
 app = FastAPI()
 app.title = 'Mi aplicacion con FastAPI'
 app.version = '0.0.1'
+
+
+class JWTBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        auth = await super().__call__(request)
+        data = validate_token(auth.credentials)
+        if data['email'] != 'admin@gmail.com':
+            raise HTTPException(status_code=403, detail='Las Credenciales son invalidas')
+
+
+class User(BaseModel):
+    email: str
+    password: str
 
 
 class Movie(BaseModel):
@@ -55,8 +70,17 @@ movies = [
 def message():
     return HTMLResponse('<h1>HOLA DESDE FASTAPI</h1>')
 
+#===========================================================
 
-@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200)
+@app.post('/login', tags=['auth'])
+def login(user: User):
+    if user.email == 'admin@gmail.com' and user.password == 'admin':
+        token: str = create_token(user.dict())
+        return JSONResponse(status_code=200, content=token)
+
+#===========================================================
+
+@app.get('/movies', tags=['movies'], response_model=List[Movie], status_code=200, dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
     return JSONResponse(status_code=200, content=movies)
 
